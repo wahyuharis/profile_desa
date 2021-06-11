105d02f3597540fc8cf920b030731b9e6a7b4499
harits wahyu <harits.wahyu.hid@gmail.com>
2021-06-11 17:47:17 +0700

    gtignore test

--- application/config/database.php
+++ application/config/database.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-/*
+/* coba ubah config
 | -------------------------------------------------------------------
 | DATABASE CONNECTIVITY SETTINGS
 | -------------------------------------------------------------------
